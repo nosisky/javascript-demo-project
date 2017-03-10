c8535d06e0891a125f17cfcada2bd4f73d03cdfa
Solomon Kingsley <johnkingzy@gmail.com>
2017-03-10 15:48:48 +0100

    i added a new function to pass my first test

--- app/library.js
+++ app/library.js
@@ -4,25 +4,41 @@ module.exports = {
 
  /* Find the average of three integers */
  computeAverage: function(num1, num2, num3) {
-   var average;
+ 	if(typeof num1 !== "number" || typeof num2 !== "number"|| typeof num3 !== "number"){
+ 		return false;
+ 	}
+ 	if(num1 === "" || num2 === "" || num3 === ""){
+ 		return false;
+ 	}
+ 	
+   var average = (num1 + num2 + num3)/3;
    return average;
  },
  
  /* Find the factorial of an integer */
  computeFactorial: function(num) {
-   var factorial;
-   return factorial;
+ 	if(typeof num !== "number" || num === ""){
+ 		return false;
+ 	}
+   return (num === 0) ? 1 : num * this.computeFactorial(num - 1);
+   
  },
 
  /* Convert a given Celcius temperature to Fahrenheit */
  convertTempCtoF: function(cTemp) {
-   var fTemp;
+ 	if(typeof cTemp != "number" || cTemp < 1){
+ 		return false;
+ 	}
+   var fTemp = (cTemp * 9/5) + 32;
    return fTemp;
  },
 
  /* Convert a given Fahrenheit temperature to Celcius */
  convertTempFtoC: function(fTemp) {
-   var cTemp;
+ 	if(typeof fTemp != "number"){
+ 		return false;
+ 	}
+   var cTemp = (fTemp  -  32)  *  5/9;
    return cTemp;
  }
 
--- tests/tests.js
+++ tests/tests.js
@@ -9,14 +9,34 @@ var myApp = require('../app/library.js');
 
 describe("Arithmetic Functionality", function() {
   it("should return 2 as average for 1, 2, 3", function() {
-    assert(myApp.computeAverage(1,2,3) == 2);
+    assert(myApp.computeAverage(1,2,3) === 2);
   })
   it("should return 5 as average for 3, 7, 5", function() {
     assert(myApp.computeAverage(3, 7, 5) == 5);
   })
   it("should return 120 as factorial for 5", function() {
-    assert(myApp.computeFactorial(5) == 120);
+    assert(myApp.computeFactorial(5) === 120);
   })
+  it("should validate output is a number", function() {
+    assert(typeof myApp.computeFactorial(5) === "number");
+  })
+  it("should check if the input is a number", function() {
+    assert(myApp.computeFactorial([]) === false);
+  })
+  it("should check if the output is greater than 1", function() {
+    assert(0 > myApp.computeFactorial(5) === false);
+  })
+  it("should validate input type", function() {
+    assert(myApp.computeFactorial("2") == false);
+  })
+  it("We check if the input is not a number", function() {
+    assert(typeof myApp.computeFactorial(1,2,3) !== "string");
+  })
+
+  it("We check if the input is not a number", function() {
+    assert.equal(false, myApp.computeFactorial());
+  })
+  
 })
 
 
@@ -24,13 +44,29 @@ describe("Temperature Conversion Functionality", function() {
   it("should return X for Celcius value Y", function() {
     assert(myApp.convertTempCtoF(40) == 104);
   })
+
   it("should return X for Celcius value Y", function() {
     assert(myApp.convertTempCtoF(60) == 140);
+  })
+  it("should check if the input is a whole number", function() {
+    assert(myApp.convertTempCtoF(60) > 1);
+  })
+   it("should check if the input is a whole number", function() {
+    assert(myApp.convertTempCtoF(-60) == false);
   })
   it("should return Y for Fahrenheit value X", function() {
     assert(myApp.convertTempFtoC(140) == 60);
   })
+  it("should validate if there is an input", function() {
+    assert(myApp.convertTempFtoC() == false);
+  })
   it("should return Y for Fahrenheit value X", function() {
     assert(myApp.convertTempFtoC(104) == 40);
   })
+
+  it("should check if output is a number", function() {
+    assert(typeof myApp.convertTempFtoC(23) === "number");
+  })
+  
+
 })
